83b152e93885646738cc41eb80e767be2dd15eb5
Katya <katya.mustafina@inmeta.no>
2016-10-19 16:58:26 +0200

    add last columns

--- pandas_to_list.py
+++ pandas_to_list.py
@@ -21,6 +21,18 @@ def pandas_to_list(original_df):
     y = original_df['A'].values.tolist()
     print(y)
 
+    print('list of last 2 column values')
+    ncols = original_df.shape[1]
+    two_last_cols_df = original_df[ columns_list[ncols-2 : ncols] ]
+
+    print('dataframe of last 2 columns of original dataframe (first 10 rows)')
+    print(two_last_cols_df.head(10))
+
+    print('last 2 columns of original dataframe')
+    print(two_last_cols_df.columns.tolist())
+    y1 = two_last_cols_df.values.tolist()
+    print(y1)
+
 if __name__ == "__main__":
 
     df = pd.DataFrame(np.random.randint(0,100,size=(100, 9)), columns=list('ABCDEFGHK'))
