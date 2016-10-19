8dcffd0bbde80040c854b7f0f5471ec855a501ca
Katya <katya.mustafina@inmeta.no>
2016-10-19 16:26:58 +0200

    add named column

--- pandas_to_list.py
+++ pandas_to_list.py
@@ -5,18 +5,21 @@ def pandas_to_list(original_df):
 
     # list of column names
     print('list of column names')
-    columns_list = df.columns.tolist()
+    columns_list = original_df.columns.tolist()
     print(columns_list)
 
     # dataframe of first 4 columns of original dataframe
     print('dataframe of first 4 columns of original dataframe (first 10 rows)')
-    first4_columns_df = df[columns_list[0:4]]
+    first4_columns_df = original_df[columns_list[0:4]]
     print(first4_columns_df.head(10))
 
     print('list of first 4 columns of original dataframe')
     x = first4_columns_df.values.tolist()
     print(x)
 
+    print('list of values a named column A')
+    y = original_df['A'].values.tolist()
+    print(y)
 
 if __name__ == "__main__":
 
