203adbb3a06e3ddfef87ca51250a96aac7b0adc1
Katya <katya.mustafina@inmeta.no>
2016-10-19 16:18:43 +0200

    convert pandas dataframe to list sample

---
+++ pandas_to_list.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+def pandas_to_list(original_df):
+
+    # list of column names
+    print('list of column names')
+    columns_list = df.columns.tolist()
+    print(columns_list)
+
+    # dataframe of first 4 columns of original dataframe
+    print('dataframe of first 4 columns of original dataframe (first 10 rows)')
+    first4_columns_df = df[columns_list[0:4]]
+    print(first4_columns_df.head(10))
+
+    print('list of first 4 columns of original dataframe')
+    x = first4_columns_df.values.tolist()
+    print(x)
+
+
+if __name__ == "__main__":
+
+    df = pd.DataFrame(np.random.randint(0,100,size=(100, 9)), columns=list('ABCDEFGHK'))
+
+    pandas_to_list(df)
+
+
+
+
