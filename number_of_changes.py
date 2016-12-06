d1ddb742a49120e51dcf28f7b9e0af7b8761c698
katyamust <katya.mustafina@gmail.com>
2016-12-07 00:08:44 +0100

    corrected version

--- number_of_changes.py
+++ number_of_changes.py
@@ -7,18 +7,19 @@ def calculate_value_change(serie):
         return 0
 
     serie_shifted = np.roll(serie, -1)
-    total_changed = np.sum(serie != serie_shifted) - 1
+
+    #total_changed = np.sum( serie[:-1] != serie_shifted[:-1] )
+    total_changed = np.count_nonzero(serie[:-1] ^ serie_shifted[:-1])
+
     return total_changed
 
-#(a and not b) or (not a and b)
-
-
 if __name__ == "__main__":
+    #x = [1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3]
+    #x = np.array([1,1,1,10,1,1,1,1,1,1,2,2,2,2,2,2,2,3,1])
 
-    #x = np.array([1,1,1,10,1,1,1,1,1,1,2,2,2,2,2,2,2,3,3])
     #x = range(1,100)
 
-   # x = pd.Series([1, 1, 1, 1, 1, 1, 1, 1, 1])
+    #x = pd.Series([1, 1, 1, 1, 1, 1, 1, 1, 1])
     x = pd.Series([1, 1, 1, 10, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3])
     n = calculate_value_change(x)
     print('Value changed in this array')
